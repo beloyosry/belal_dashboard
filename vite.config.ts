c2b3d0525a142913b403e8f791eec5ec5514c364
beloyosry <byosry@gmail.com>
2025-04-26 20:38:19 +0300

    try to fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -18,9 +17,7 @@ export default defineConfig(({ mode }) => {
 
     console.log(`API target: ${apiTarget} (useLocalApi: ${useLocalApi})`);
     return {
-        plugins: [react(), mode === "development" && componentTagger()].filter(
-            Boolean
-        ),
+        plugins: [react()],
         server: {
             proxy: {
                 "/api": {
